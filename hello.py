54faeb6d690ec9c929c13002b3b97cec1f7b2280
Toey Tui <toeytui@gmail.com>
2026-01-02 05:18:03 +0000

    add workflow

--- hello.py
+++ hello.py
@@ -8,3 +8,18 @@ def hello():
   
 # test funciton
 hello()
+
+## workflow for github
+
+#1. create repo on github
+#2. copy github url to posit/another software
+#3. create new file
+#4. git status
+#5. git add .
+#6. git commit -m “message”
+#7. git push / pull
+
+
+
+
+
